c1d1217db9e197854ad63e7e6f00ccaea5255bc3
Jan Portisch <janportisch@vpn-132-018.rz.uni-mannheim.de>
2020-04-15 17:59:54 +0200

    Surefire plugin added.

--- src/test/java/IntegerTupleTest.java
+++ src/test/java/IntegerTupleTest.java
@@ -1,10 +1,11 @@
 import java.util.HashSet;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class IntegerTupleTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void testEquals() {
         assertEquals(new IntegerTuple(1, 1), new IntegerTuple(1, 1));
         assertEquals(new IntegerTuple(1, 2), new IntegerTuple(2, 1));
@@ -16,6 +17,5 @@ class IntegerTupleTest {
         testSet.add(new IntegerTuple(Integer.MAX_VALUE, 1));
         testSet.add(new IntegerTuple(1, Integer.MAX_VALUE));
         assertTrue(testSet.size() == 2, "Expected size: 1 | Actual size: " + testSet.size());
-
     }
 }
